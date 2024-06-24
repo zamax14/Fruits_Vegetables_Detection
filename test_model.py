798cda57e4e863ba376d78bffb680c07e2115351
zamax14 <zarate.alejandroM14@live.com.mx>
2024-06-23 22:16:00 -0600

    [Feat] Update scripts to test/export model

--- test_model.py
+++ test_model.py
@@ -1,5 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/FV_model4/weights/best.pt")
 
-model.predict("data/fruits_and_vegetables/new_test/Screenshot from 2024-06-19 15-06-24.png", save=True, imgsz=640, conf=0.5)
+model = YOLO("./data/models/best.pt")
+
+results = model(["im1.jpg", "im2.jpg"])
+
+for result in results:
+    result.show() 
