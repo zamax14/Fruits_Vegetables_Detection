abc4563ff0592766a103f2e34445ce62e9fb04a4
zamax14 <zarate.alejandroM14@live.com.mx>
2024-08-06 12:03:22 -0600

    [Feat] Create data.yaml automatically

---
+++ create_yaml_cfg.py
@@ -0,0 +1,33 @@
+import os
+import yaml
+
+current_dir = os.getcwd()
+
+data = {
+    'train': os.path.join(current_dir, 'data/fruits_and_vegetables/train'),
+    'val': os.path.join(current_dir, 'data/fruits_and_vegetables/val'),
+    'nc': 18,
+    'names': [
+        'aguacate', 
+        'brocoli', 
+        'calabacita', 
+        'cebolla', 
+        'chile', 
+        'fresa', 
+        'jitomate', 
+        'lechuga', 
+        'limon', 
+        'mango', 
+        'manzana', 
+        'naranja', 
+        'papa', 
+        'pepino', 
+        'pinia', 
+        'platano', 
+        'sandia', 
+        'zanahoria'
+    ]
+}
+
+with open('data/fruits_and_vegetables/data.yaml', 'w') as file:
+    yaml.dump(data, file, default_flow_style=False)
